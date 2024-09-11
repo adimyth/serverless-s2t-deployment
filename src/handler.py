3d6ed6c736c97c52cf0962db32b2550a6ec47a7b
adimyth <aditya@fnp.dev>
2024-09-11 15:03:14 +0530

    renames the secret key

--- src/handler.py
+++ src/handler.py
@@ -34,7 +34,7 @@ def get_model(language):
             model=model_name,
             chunk_length_s=30,
             device=device,
-            token=os.getenv("RUNPOD_HF_API_KEY"),
+            token=os.getenv("RUNPOD_SECRET_HF_API_KEY"),
         )
         transcriber.model.config.forced_decoder_ids = (
             transcriber.tokenizer.get_decoder_prompt_ids(
@@ -49,7 +49,7 @@ def get_model(language):
             chunk_length_s=30,
             model=model_name,
             device=device,
-            token=os.getenv("RUNPOD_HF_API_KEY"),
+            token=os.getenv("RUNPOD_SECRET_HF_API_KEY"),
         )
 
 
